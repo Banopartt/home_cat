cdad886d2f073587075d10a88a6a57d09d9ae142
Banopartt <turgaliev.almat11@gmail.com>
2025-04-11 22:15:16 +0600

    finished banner-info

---
+++ src/components/banner-info.vue
@@ -0,0 +1,60 @@
+<template>
+    <section class="hero  md:bg-[url(../assets/images/banner-img.png)] object-cover h-svh bg-no-repeat">
+      <div class="hero-content">
+        <div class="text-block">
+          <p>Санкт-Петербург</p>
+          <h1>Котейка</h1>
+          <p>Уютная гостиница для котов и кошек</p>
+          <button>Забронировать</button>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  .hero {
+    padding: 100px 40px 60px;
+  }
+  
+  .hero-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding-left: 198px;
+  }
+  
+  .text-block {
+    max-width: 400px;
+  }
+  
+  .text-block h1 {
+    font-size: 3rem;
+    margin-bottom: 10px;
+    color:#1A212F;
+    font-weight: 500;
+  }
+  
+  .text-block p {
+    font-size: 1.2rem;
+    margin-bottom: 20px;
+    color: #1A212F;
+  }
+  
+  button {
+    background-color:white;
+    color:#1A212F;
+    border: none;
+    padding: 12px 24px;
+    border-radius: 8px;
+    font-size: 1rem;
+    cursor: pointer;
+    border-radius: 25px;
+  }
+  
+
+  </style>
---
+++ src/components/widgets/navbar-widget.vue
@@ -0,0 +1,43 @@
+<template>
+    <header class="header">
+        <div class="logo">
+            Котейка
+        </div>
+        <nav class="hidden md:flex gap-7 items-center">
+            <a href="why">Почему мы?</a>
+            <a href="rooms">Номера</a>
+            <a href="reviews">Отзывы</a>
+            <a href="contact">Как нас найти</a>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 40px;
+    width: 100%;
+}
+
+.logo {
+    font-size: 20px;
+    text-decoration: none;
+    color: #1A212F;
+    font-weight: 500;
+    transition: color 0.2s;
+}
+
+.nav a:hover {
+    color: #1A212F;
+}
+
+a {
+    color: #1A212F;
+}
+</style>
